df39899fb51b8fe688caf9a566bba70298a8a42b
jtschuwirth <jtschuwirth@uc.cl>
2025-03-18 23:20:12 -0300

    remove disabled accounts from uptime

--- functions/getQuestingUptime.py
+++ functions/getQuestingUptime.py
@@ -3,16 +3,18 @@ from dfk_commons.classes.TablesManager import TablesManager
 def getQuestingUptimeByProfession(tablesManager: TablesManager, profession: str):
 
     allAccounts = tablesManager.accounts.scan(
-        FilterExpression="enabled_quester=:enabled_quester AND profession=:profession",
+        FilterExpression="enabled_quester=:enabled_quester AND profession=:profession AND disabled <> :disabled",
         ExpressionAttributeValues={
             ":enabled_quester": True,
+            ":disabled": True,
             ":profession": profession
         }
     )["Items"]
     questingAccounts = tablesManager.accounts.scan(
-        FilterExpression="questing=:questing AND profession=:profession",
+        FilterExpression="questing=:questing AND profession=:profession AND disabled <> :disabled",
         ExpressionAttributeValues={
             ":questing": True,
+            ":disabled": True,
             ":profession": profession
         }
     )["Items"]
