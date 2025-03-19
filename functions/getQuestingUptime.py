723654d3ed29f01d8a0264f83038965f8fb98c88
jtschuwirth <jtschuwirth@uc.cl>
2025-03-18 23:32:46 -0300

    changed not_disabled var on uptime

--- functions/getQuestingUptime.py
+++ functions/getQuestingUptime.py
@@ -3,18 +3,18 @@ from dfk_commons.classes.TablesManager import TablesManager
 def getQuestingUptimeByProfession(tablesManager: TablesManager, profession: str):
 
     allAccounts = tablesManager.accounts.scan(
-        FilterExpression="enabled_quester=:enabled_quester AND profession=:profession AND disabled <> :disabled",
+        FilterExpression="enabled_quester=:enabled_quester AND profession=:profession AND disabled <> :not_disabled",
         ExpressionAttributeValues={
             ":enabled_quester": True,
-            ":disabled": True,
+            ":not_disabled": True,
             ":profession": profession
         }
     )["Items"]
     questingAccounts = tablesManager.accounts.scan(
-        FilterExpression="questing=:questing AND profession=:profession AND disabled <> :disabled",
+        FilterExpression="questing=:questing AND profession=:profession AND disabled <> :not_disabled",
         ExpressionAttributeValues={
             ":questing": True,
-            ":disabled": True,
+            ":not_disabled": True,
             ":profession": profession
         }
     )["Items"]
